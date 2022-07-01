78a6ae6a72ac6639e7e60b6e5ee9c8ab62554a1f
aires1294 <pedrotja.engenharia@gmail.com>
2022-07-01 10:24:54 -0300

    exercicio 4 finalizado

--- src/challenges.js
+++ src/challenges.js
@@ -1,50 +1,46 @@
 // Desafio 1 FEITOOOOOOOOO
-  function compareTrue(a, b) {
-    let x = 'true'
-    let y = 'false'
-    if (a === true && b === true){
-      return x;
-    } else {
-      return y;
-    }
-   
+function compareTrue(a, b) {
+  let x = 'true';
+  let y = 'false';
+  if (a === true && b === true) {
+    return x;
+  } else {
+    return y;
   }
-  /*console.log(compareTrue(girafa = true, elefante = false));*/
-
+}
+/*console.log(compareTrue(girafa = true, elefante = false));*/
 
 // Desafio 2 FEITOOOOOOO
 function calcArea(base, height) {
   let altura = (base * height) / 2;
   return altura;
-  
 }
 
 /*console.log(calcArea(10, 5));
-console.log(calcArea(50, 5));*/
+            console.log(calcArea(50, 5));*/
 
 // Desafio 3 FEITOOOOOOOOOOO
 function splitSentence(sentence) {
-    let arrayNovo = [];
-    arrayNovo = sentence.split(" ");
-    return arrayNovo;
-    
+  let arrayNovo = [];
+  arrayNovo = sentence.split(' ');
+  return arrayNovo;
 }
 /*console.log(splitSentence('go Trybe'));*/
 
+// Desafio 4 FEITOOOOOOO
 
-// Desafio 4  
 function concatName(array) {
   let arrayNovo = [];
-  for (index = 0; index < array.length; index+=1){
-    arrayNovo.push = array[]
-  }
-  // seu código aqui
+  arrayNovo = array[array.length -1] + ' '+array[0];
+
+  return arrayNovo;
 }
+/*console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));*/
 
 // Desafio 5 FEITOOOOOOO
 function footballPoints(wins, ties) {
   let somaPontos = 0;
-  somaPontos = (wins * 3) + ties;
+  somaPontos = wins * 3 + ties;
   return somaPontos;
 }
 /*console.log(footballPoints(14, 8));*/
@@ -53,24 +49,16 @@ function footballPoints(wins, ties) {
 function highestCount(arrayNumeros) {
   var contador = 0;
   arrayNumeros.sort();
-  
-  for (index = 0; index < arrayNumeros.length; index += 1){
-    
-    if (arrayNumeros[index] === arrayNumeros[arrayNumeros.length - 1]){
-      
-    contador = contador + 1;
-   
-  } 
-  
+
+  for (index = 0; index < arrayNumeros.length; index += 1) {
+    if (arrayNumeros[index] === arrayNumeros[arrayNumeros.length - 1]) {
+      contador = contador + 1;
+    }
   }
   /*console.log(contador);*/
   return contador;
-  
 }
 /*console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));*/
-
-
-
 
 // Desafio 7
 function catAndMouse() {

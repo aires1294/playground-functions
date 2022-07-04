28f599969c43d6ba385b169f8a2d6b4333edb850
aires1294 <pedrotja.engenharia@gmail.com>
2022-07-04 17:58:46 -0300

    tirei aspas do reutrn

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,9 +6,9 @@ function generatePhoneNumber() {
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB && lineA > Math.abs(lineB) - Math.abs(lineC) && lineB > Math.abs(lineA) - Math.abs(lineC) && lineC > Math.abs(lineB) - Math.abs(lineA)){
-      return 'true';
+      return true;
   } else {
-      return 'false';
+      return false;
   }
 }
 console.log(triangleCheck(10, 14, 8));
